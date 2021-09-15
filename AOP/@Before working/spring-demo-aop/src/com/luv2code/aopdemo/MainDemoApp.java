6cf143c254c52437c625c624f200cd472866d6bb
Ganesh Jadhav <67518287+Anddy-96@users.noreply.github.com>
2021-09-15 23:35:19 +0530

    Add files via upload

---
+++ AOP/@Before working/spring-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -0,0 +1,38 @@
+package com.luv2code.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//read spring config class
+		
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		
+		//get the bean from the spring container
+		
+		AccountDAO theAccount = context.getBean("accountDAO", AccountDAO.class);
+		
+		//get the bean for membership class
+		MembershipDAO themember = context.getBean("membershipDAO", MembershipDAO.class);
+		
+		//call the business method
+		
+		
+		theAccount.addAccount();
+		
+		themember.addAccount();
+		//close the context
+		context.close();
+		
+		
+	}
+
+}
