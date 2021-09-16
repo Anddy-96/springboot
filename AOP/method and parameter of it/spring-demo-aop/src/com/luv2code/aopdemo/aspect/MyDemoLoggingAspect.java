deddf39a5a51bbe08e078a76618b1f299aff2e01
Ganesh Jadhav <67518287+Anddy-96@users.noreply.github.com>
2021-09-17 00:32:45 +0530

    Add files via upload

---
+++ AOP/method and parameter of it/spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,28 @@
+package com.luv2code.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+	
+	//this is where we add all of our related advices for logging
+	//let's start with @before advice
+	@Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	public void beforeAddAccount()
+	{
+		System.out.println(getClass() +": this is running before account method");
+	}
+
+	
+	
+	
+	
+	@Before("execution(public void updateAccount())")
+	public void beforeupdateAccount()
+	{
+		System.out.println(getClass() +": this is running before update account method");
+	}
+}
