c466203822fa731b79cec75ebc18bef1c96a2f45
Ganesh Jadhav <67518287+Anddy-96@users.noreply.github.com>
2021-09-28 23:40:52 +0530

    Add files via upload

--- Spring-REST/Spring-rest-controller/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ Spring-REST/Spring-rest-controller/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -5,7 +5,10 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +17,20 @@ import com.luv2code.springdemo.entity.Student;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+	
+	private List<Student> thestud;
+	
+	@PostConstruct
+	public void loadData()
+	{
+		 thestud = new ArrayList<>();
+		 
+		 thestud.add(new Student("ALEX", "Minnor"));
+		 thestud.add(new Student("Beka", "Dash"));
+		 thestud.add(new Student("Bob", "Bally"));
+		
+	}
+	
 
 	
 	//define endpoitnn for /students and return students 
@@ -21,15 +38,15 @@ public class StudentRestController {
 	@GetMapping("/students")
 	public List<Student> getStudent()
 	{
-		 List<Student> thestud = new ArrayList<>();
-		 
-		 thestud.add(new Student("ALEX", "Minnor"));
-		 thestud.add(new Student("Beka", "Dash"));
-		 thestud.add(new Student("Bob", "Bally"));
-		
-		 
-		 
 		return thestud;
+	}
+	
+	//define endpoint for "/students{studentid}" -return student at index
+	@GetMapping("/students/{studid}")
+	public Student getStudent(@PathVariable int studid)
+	{
+		//just indexing the list 	
+		return thestud.get(studid);
 	}
 	
 }
