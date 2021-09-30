db2f8b2b6a8a0de918c2bddb42d194b6a6d0cbc7
Ganesh Jadhav <67518287+Anddy-96@users.noreply.github.com>
2021-10-01 00:48:29 +0530

    Add files via upload

---
+++ spring-boot-projects/spring-boot-1stproject/myspringcoolapp/myspringcoolapp/src/test/java/com/luv2code/springboot/demo/myspringcoolapp/MyspringcoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.demo.myspringcoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringcoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
