8cc7c76cb0a7a37a21287a68e4b21006ce9d1099
Ganesh Jadhav <67518287+Anddy-96@users.noreply.github.com>
2021-10-01 01:29:04 +0530

    Add files via upload

---
+++ spring-boot-projects/Spring-boot-dev-tools/02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/myspringcoolapp/MyspringcoolappApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.demo.myspringcoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringcoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringcoolappApplication.class, args);
+	}
+
+}
